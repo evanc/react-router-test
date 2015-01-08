7b21a21b17e5e1dec39b9855f9c5d77e4b018cfe
evanc <evan@paloalto.com>
2015-01-08 12:50:07 -0800

    use the router the right way

--- src/scripts/components/Forecast.jsx
+++ src/scripts/components/Forecast.jsx
@@ -8,60 +8,14 @@ var React = require('react/addons');
 var Router = require('react-router');
 var {DefaultRoute, Route, Routes, RouteHandler, Link} = Router;
 
-var SuperLocation = require('./SuperLocation');
-
-var Expenses = require('./Expenses');
-var Revenue = require('./Revenue');
 
 require('../../styles/Forecast.less');
 
-var Page = React.createClass({
-    render: function () {
-        return (
-            <div>
-                WOWOWOWOWOW!
-                <Link to="expenses">Expenses</Link><br />
-                <Link to="revenue">Revenue</Link><br />
-
-                <RouteHandler />
-            </div>
-        );
-    }
-});
 
 var Forecast = React.createClass({
-    getInitialState: function () {
-        return {
-            loaded: 'nope',
-            route: <div>not navigated yet</div>
-        };
-    },
-
-    routeHandler: SuperLocation,
-
-    setRoutes: function() {
-        var routes = (
-            <Route path="/" handler={Page}>
-                <DefaultRoute handler={Expenses} />
-                <Route name="expenses" handler={Expenses} />
-                <Route name="revenue" handler={Revenue} />
-            </Route>
-        );
-
-        var that = this;
-
-        console.log('about to run');
-
-        Router.run(routes, this.routeHandler, function (handler) {
-            that.setState({
-                loaded: 'yeah',
-                route: handler
-            });
-        });
-    },
 
     componentDidMount: function() {
-        this.setRoutes();
+
     },
 
     componentDidUpdate: function(prevProps, prevState) {
@@ -69,14 +23,13 @@ var Forecast = React.createClass({
     },
 
     render: function () {
-        if (this.state.loaded === 'yeah') {
-            // debugger
-        }
+
         return (
             <div>
                 XXXD<br />
-                {this.state.loaded}
-                {this.state.route}
+                <Link to="forecast/expenses">Expenses</Link><br />
+                <Link to="forecast/revenue">Revenue</Link>
+                <RouteHandler />
             </div>
         );
         }
--- src/scripts/components/main.jsx
+++ src/scripts/components/main.jsx
@@ -8,14 +8,23 @@ var {DefaultRoute, Route, Routes} = Router;
 var Forecast = require('./Forecast');
 var Scoreboard = require('./Scoreboard');
 
+var Expenses = require('./Expenses');
+var Revenue = require('./Revenue');
+
 var routes = (
     <Route path="/" handler={RacktTestApp}>
         <DefaultRoute handler={Forecast}></DefaultRoute>
-        <Route name="forecast" path="forecast/" handler={Forecast}></Route>
-        <Route name="scoreboard" path="scoreboard/" handler={Scoreboard}></Route>
+        <Route name="forecast" path="/forecast" handler={Forecast}>
+            <Route name="forecast/expenses" path="/expenses" handler={Expenses} />
+            <Route name="forecast/revenue" path="/revenue" handler={Revenue} />
+        </Route>
+        <Route name="scoreboard" path="/scoreboard" handler={Scoreboard}>
+            <Route name="scoreboard/expenses" path="/expenses" handler={Expenses} />
+            <Route name="scoreboard/revenue" path="/revenue" handler={Revenue} />
+        </Route>
     </Route>
 );
 
-Router.run(routes, function (Handler) {
+Router.run(routes, function (Handler, state) {
     React.render(<Handler />, document.getElementById('content'));
 });
