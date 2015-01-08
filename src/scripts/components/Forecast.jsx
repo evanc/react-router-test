2ed747ecbf62d9ddcea930710944a426e69b9ae0
evanc <evan@paloalto.com>
2015-01-08 14:07:31 -0800

    doesn't quite work

--- src/scripts/components/Forecast.jsx
+++ src/scripts/components/Forecast.jsx
@@ -6,18 +6,59 @@
 
 var React = require('react/addons');
 var Router = require('react-router');
-var {DefaultRoute, Route, Routes, RouteHandler, Link, State} = Router;
+var {DefaultRoute, Route, Routes, RouteHandler, Link, State, NotFoundRoute} = Router;
 
 
 require('../../styles/Forecast.less');
 
 
+var FakePage = React.createClass({
+    render: function () {
+        return (
+            <div>
+                fake page
+                <RouteHandler />
+            </div>
+        );
+    }
+});
+
+var Page = React.createClass({
+    render: function () {
+        return (
+            <div>
+                XXXD<br />
+                <Link to="diplo/forecast">Expenses</Link><br />
+                <Link to="forecast/revenue">Revenue</Link>
+                <RouteHandler />
+            </div>
+        );
+    }
+});
+
 var Forecast = React.createClass({
 
     mixins: [State],
 
-    componentDidMount: function() {
+    getInitialState: function() {
+        return {
+            router: <span>No.</span>
+        };
+    },
 
+    componentDidMount: function() {
+        var routes = <Route path="/" handler={FakePage}>
+            <Route name="diplo/forecast" path="forecast" handler={Page} />
+            <NotFoundRoute name="not-found" handler={FakePage} />
+        </Route>
+
+        var that = this;
+
+        Router.run(routes, function (router) {
+            that.setState({
+                router: router
+            });
+        });
     },
 
     componentDidUpdate: function(prevProps, prevState) {
@@ -26,12 +67,13 @@ var Forecast = React.createClass({
 
     render: function () {
 
+        console.log('rendering', this.state.router);
+
         return (
             <div>
-                XXXD<br />
-                <Link to="forecast/expenses">Expenses</Link><br />
-                <Link to="forecast/revenue">Revenue</Link>
-                <RouteHandler />
+                L
+                {this.state.router}
+                M
             </div>
         );
         }
